1240e09a18448fb815ff241fe60940cca5616382
caradja <67961246+caradja@users.noreply.github.com>
2023-04-21 20:29:41 +0200

    Update final.py

--- final.py
+++ final.py
@@ -30,7 +30,7 @@ conn = sqlite3.connect('ecsel_database.db')
 df_grants = pd.read_sql(f"SELECT year, grants FROM grants WHERE country = '{country}' ", conn)
 df_grants = df_grants.set_index('year')
 df_participants = pd.read_sql(f"SELECT * FROM participants WHERE country = '{country}' ", conn)
-df_coordinators = pd.read_sql(f"SELECT * FROM coordinators WHERE country = '{country}' ", conn)
+#df_coordinators = pd.read_sql(f"SELECT * FROM coordinators WHERE country = '{country}' ", conn)
 conn.close()
 
 # grants
@@ -40,7 +40,3 @@ st.bar_chart(df_grants)
 # participants
 st.subheader(f'Participants in {ct[country]}')
 st.dataframe(df_participants)
-
-# coordinators
-st.subheader(f'Project cooordinators in {ct[country]}')
-st.dataframe(df_coordinators)
