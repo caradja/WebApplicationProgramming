a5f724d65484f6c83b6c14264a0562af4865395c
caradja <67961246+caradja@users.noreply.github.com>
2023-04-21 20:17:57 +0200

    Update final.py

--- final.py
+++ final.py
@@ -5,3 +5,8 @@ from sqlite3 import connect
 st.write("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
+
+conn = sqlite3.connect('ecsel_database.db')
+cur = conn.cursor()
+cur.execute('SELECT Country FROM Countries')
+df = pd.DataFrame(cur.fetchall(), columns = ['COUNTRYNAME'])
