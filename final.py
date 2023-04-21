dba6c5d92973c967e35cf4b49436348c949965c5
caradja <67961246+caradja@users.noreply.github.com>
2023-04-21 20:48:28 +0200

    Update final.py

--- final.py
+++ final.py
@@ -21,7 +21,7 @@ from PIL import Image
 # Title
 image = Image.open('logo.jpg')
 resized_image = image.resize((325, 192))
-st.image(rezised_image)
+st.image(resized_image)
 st.title("Partner search")
 
 # Select country
