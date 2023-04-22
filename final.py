a497d0571b0a513498bd7e2693b83cff56c45153
caradja <67961246+caradja@users.noreply.github.com>
2023-04-22 19:43:09 +0200

    Update final.py

--- final.py
+++ final.py
@@ -20,10 +20,26 @@ logo.thumbnail((400, 400))
 # Create a container and center the logo image
 container = st.beta_container()
 with container:
-    st.markdown(
-        f'<div style="display: flex; justify-content: center;">{logo}</div>',
-        unsafe_allow_html=True,
-    )
+    col1, col2, col3 = st.beta_columns(3)
+    col2.image(logo, width=300)
+    col1.empty()
+    col3.empty()
+
+# Add CSS styling to center the container
+container.markdown(
+    f"""
+    <style>
+    .element-container:nth-child(3) {{
+        display: flex;
+        justify-content: center;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+    
+    
+    
 # Display the title
 st.title("Partner search")
 
